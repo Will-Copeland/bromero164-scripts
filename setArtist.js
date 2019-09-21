be0c31f603fa8258ba7bd4937b97cea53a22fe04
Will-Copeland <will@localhost.localdomain>
2019-09-21 13:41:21 -0700

    fin

--- setArtist.js
+++ setArtist.js
@@ -7,7 +7,6 @@ const inquirer = require('inquirer');
 
 const directoryPath = process.cwd();
 const fileArr = [];
-const notChanged = [];
 
 
 function getQuestion(title1, title2, index) {
@@ -23,46 +22,116 @@ function getQuestion(title1, title2, index) {
   return question;
 }
 
+function getMp3Files(files) {
+  return files.filter(file => file.includes('.mp3'));
+}
 
-fs.readdir(directoryPath, (err, files) => {
-  if (err) {
-    return console.log(chalk.bgRed(`Unable to scan directory: ${chalk.red(err)}`));
-  }
+function getConflicts(mp3Files) {
   let num = 0;
-  const conflicts = files.map((file) => {
+  const conflictFiles = [];
+
+  const quests = mp3Files.map((file, index) => {
     const metaData = NodeID3.read(file);
     const [artist, title] = file.split(' - ');
-  
 
     if (file.includes('.mp3') && file.includes(' - ')) {
       fileArr.push(file);
-      if (metaData.title && metaData.artist) {
+      if (metaData.title && metaData.artist && !!title) {
         const titleStripped = title.slice(0, title.indexOf('.mp3'));
         // console.log(titleStripped != metaData.title);
         if (titleStripped !== metaData.title) {
-          console.log(num);
-          console.log(metaData);
-          
+          conflictFiles.push(file);
           const quest = getQuestion(titleStripped, metaData.title, num);
           num++;
           return quest;
         }
       }
     }
-    notChanged.push(file);
   });
 
+  return {
+    conflictFiles,
+    quests,
+  };
+}
 
-  const questions = conflicts.filter(con => con !== undefined);
-  console.log(questions);
+function getAllFiles() {
+  return fs.readdirSync(directoryPath);
+}
 
-  inquirer.prompt(questions)
-    .then((answers) => {
-      console.log(answers);
-    });
+
+fs.readdir(directoryPath, (err, files) => {
+
 
   // notChanged.push(file);
 
 
   // console.log(chalk.white(`${notChanged.length} files will not be changed`));
 });
+
+function main() {
+  const allFiles = getAllFiles();
+  const mp3s = getMp3Files(allFiles);
+  const conflicts = getConflicts(mp3s);
+  const questions = conflicts.quests.filter(con => con !== undefined);
+  const filesToChange = mp3s.filter(file => !conflicts.conflictFiles.includes(file));
+
+
+  filesToChange.forEach((file) => {
+    const [artist, title] = file.split(' - ');
+    if (artist === undefined || title === undefined) return;
+    if (!file || file === undefined) return;
+    const titleStripped = title.slice(0, title.indexOf('.mp3'));
+
+    console.log('Reading: ', file);
+
+    const metaData = NodeID3.read(file);
+    console.log('READ: ', metaData.raw);
+
+    if (!metaData.title || !!metaData.title.length) {
+      console.log('UPDATING: ');
+
+      NodeID3.update({
+        title,
+      }, file);
+    }
+    if (!metaData.artist || !!metaData.artist.length) {
+      NodeID3.update({
+        artist,
+      }, file);
+    }
+
+
+    fs.renameSync(file, titleStripped);
+  });
+
+  inquirer.prompt(questions)
+    .then((answers) => {
+      const files = conflicts.conflictFiles;
+
+      files.forEach((file, index) => {
+        const [artist, title] = file.split(' - ');
+
+        if (artist === undefined || title === undefined) return;
+
+        const metaData = NodeID3.read(file);
+
+        if (!metaData.title || !!metaData.title.length) {
+          NodeID3.update({
+            title: answers[index],
+          }, file);
+        }
+        if (!metaData.artist || !!metaData.artist.length) {
+          NodeID3.update({
+            artist,
+          }, file);
+        }
+        fs.renameSync(file, answers[index]);
+      });
+    });
+
+
+  console.log(chalk.bgWhite('DONE!'));
+}
+
+main();
